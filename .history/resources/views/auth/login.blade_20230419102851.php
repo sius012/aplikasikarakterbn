28118aadb8f4f99acd6d2f47bb9dae9d14d39e86
sius012 <dezoditing012@gmail.com>
2023-04-19 10:30:22 +0700

    login

--- .history/resources/views/auth/login.blade_20230419102851.php
+++ .history/resources/views/auth/login.blade_20230419102851.php
@@ -45,7 +45,7 @@
                             <div class="card card-plain mt-8">
                                 <div class="card-header pb-0 text-left bg-transparent">
                                     <h3 class="font-weight-bolder ">Masukan sebagai Guru</h3>
-                                    <p class="mb-0">Masukan email dan kata sandi yang telah terdaftar</p>
+                                    <p class="mb-0">Enter your email and password to sign in</p>
                                 </div>
                                 <form method="POST" action="{{ route('login') }}">
                                     @csrf
@@ -59,10 +59,20 @@
                                         <div class="mb-3">
                                             <input type="password" name="password" required autofocus class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
                                         </div>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+                                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                                        </div>
                                         <div class="text-center">
                                             <button type="submit" class="btn btn-primary w-100 mt-4 mb-0">Sign in</button>
                                         </div>
                                   
+                                </div>
+                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                                    <p class="mb-4 text-sm mx-auto">
+                                        Don't have an account?
+                                        <a href="javascript:;" class="text-info text-gradient font-weight-bold">Sign up</a>
+                                    </p>
                                 </div>
                             </form>
                             </div>
@@ -80,6 +90,60 @@
       
         </section>
     </main>
+    <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+    <footer class="footer py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mb-4 mx-auto text-center">
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                        Company
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                        About Us
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                        Team
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                        Products
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                        Blog
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                        Pricing
+                    </a>
+                </div>
+                <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                        <span class="text-lg fab fa-dribbble"></span>
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                        <span class="text-lg fab fa-twitter"></span>
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                        <span class="text-lg fab fa-instagram"></span>
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                        <span class="text-lg fab fa-pinterest"></span>
+                    </a>
+                    <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                        <span class="text-lg fab fa-github"></span>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8 mx-auto text-center mt-1">
+                    <p class="mb-0 text-secondary">
+                        Copyright © <script>
+                            document.write(new Date().getFullYear())
+
+                        </script> Soft by Creative Tim.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
     <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
     <!--   Core JS Files   -->
     <script src="{{asset('ui-assets/js/core/popper.min.js')}}"></script>
--- .history/resources/views/auth/login.blade_20230419102945.php
+++ .history/resources/views/auth/login.blade_20230419102945.php
@@ -59,6 +59,10 @@
                                         <div class="mb-3">
                                             <input type="password" name="password" required autofocus class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
                                         </div>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+                                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                                        </div>
                                         <div class="text-center">
                                             <button type="submit" class="btn btn-primary w-100 mt-4 mb-0">Sign in</button>
                                         </div>
